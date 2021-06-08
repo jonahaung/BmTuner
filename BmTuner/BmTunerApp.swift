3b53d7547125244bf03581dabba22862ccafb4f3
Aung Ko Min <jonahaung@gmail.com>
2021-06-08 11:47:23 +0800

    Initial Commit

---
+++ BmTuner/BmTunerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BmTunerApp.swift
+//  BmTuner
+//
+//  Created by Aung Ko Min on 8/6/21.
+//
+
+import SwiftUI
+
+@main
+struct BmTunerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BmTuner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BmTuner
+//
+//  Created by Aung Ko Min on 8/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
